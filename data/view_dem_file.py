106fd5bd21954b7c548eb32c0f64baf51293c209
Andrew Houghton <houghtonandrew0@gmail.com>
2019-02-01 09:36:09 +1100

    changed file path to allow it to be run from any directory

--- data/view_dem_file.py
+++ data/view_dem_file.py
@@ -1,12 +1,16 @@
 import tifffile as tiff
 from PIL import Image
 import bz2
+import os
 
-# unzip
-data=bz2.decompress(open('dem.tif.bz2','rb').read())
-with open('dem2.tif','wb') as handle:
+path=os.path.dirname(os.path.abspath(__file__))+'/'
+
+data=bz2.decompress(open(path+'dem.tif.bz2','rb').read())
+with open(path+'dem2.tif','wb') as handle:
     handle.write(data)
 
-im=tiff.imread('dem2.tif')
+im=tiff.imread(path+'dem2.tif')
 im2 = im.astype('int32')
+print(im2.max())
+print(im2.min())
 Image.fromarray(im2,'I').show()
