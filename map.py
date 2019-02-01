0434db9b8ee78d635cc185010cb5104522b07d2d
Andrew Houghton <houghtonandrew0@gmail.com>
2019-02-01 17:09:30 +1100

    refactoring map.py

--- map.py
+++ map.py
@@ -1,54 +1,51 @@
-
-def add_location_to_list(data):
-
-
-def to_node(row, col, altitude):
-    node = Node()
-    node.altitude = altitude
-    node.original_location.add((row, col))
-    return node
+from node import Node
 
 
-def create_nodes(height_map):
-    return [[to_node(i, j, height_reading) for j, height_reading in enumerate(row)]
-        for i, row in enumerate(height_map)]
+def map_with_index(func, data):
+    return [[func(i, j, item) for j, item in enumerate(row)] for i, row in enumerate(data)]
 
 
-def within_array_bounds(array, index):
-    if index[0] < 0 or index[1] < 0:
-        return False
-    if index[0] >= len(array):
-        return False
-    if index[1] >= len(array[0]):
-        return False
-    return True
-
-
-def connect_node(nodes, row, col):
-    adjacent_coordinates = [
-        (row - 1, col),
-        (row + 1, col),
-        (row, col - 1),
-        (row, col + 1)]
-    neighbours = [nodes[coord[0]][coord[1]]
-                  for coord in adjacent_coordinates if within_array_bounds(nodes, coord)]
-
-    for neighbour in neighbours:
-        add_neighbour(nodes[row][col], neighbour)
-    nodes[row][col].border = len(neighbours) != 4
-
-
-def connect_nodes(nodes):
-    for row in range(len(nodes)):
-        for col in range(len(nodes[row])):
-            connect_node(nodes, row, col)
-
-
+def flatten_list(list_of_lists):
+    return sum(list_of_lists, [])
 
 
 class LocationGraph:
     def __init__(self, height_map):
-        self.node_grid = create_nodes(height_grid)
+        self.node_grid = map_with_index(to_node, height_map)
         self.connect_nodes()
-    
-    def connect_nodes():
+        self.node_list = flatten_list(self.node_grid)
+        self.flag_borders()
+
+    def to_node(self, row, col, altitude):
+        node = Node()
+        node.altitude = altitude
+        node.original_location.add((row, col))
+        return node
+
+    def add_downstream_flow(self, higher_node, lower_node):
+        node.outflow.add(neighbour)
+        neighbour.inflow.add(node)
+
+    def add_neighbour(self, node, neighbour):
+        if node.altitude > neighbour.altitude:
+            add_downstream_flow(node, neighbour)
+        else:
+            add_downstream_flow(neighbour, node)
+
+    def connect_node(self, row, col, item):
+        adjacent_coordinates = [
+            (row - 1, col),
+            (row + 1, col),
+            (row, col - 1),
+            (row, col + 1)
+        ]
+
+        for row, col in adjacent_coordinates:
+            try:
+                add_neighbour(item, self.node_grid[row][col])
+            except IndexError as e:
+                pass
+
+    def connect_nodes(self):
+        map_with_index(self.connect_node, self.node_grid)
+        # TODO set borders
