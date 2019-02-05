a0ca918c8ee12f0b59ee4b673e6d4524ad0e0bd1
Andrew Houghton <houghtonandrew0@gmail.com>
2019-02-05 16:13:35 +1100

    Exposed error, set does not match nodes correctly

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -7,32 +7,34 @@ class TestGraph(unittest.TestCase):
     def all_connections_both_directions(self, graph):
         for node in graph.ascending():
             for i in node.inflow:
+                print(f'testing if\n{node} is in \n{i.outflow}')
+                print(f'{node in i.outflow}')
                 self.assertTrue(node in i.outflow)
             for i in node.outflow:
                 self.assertTrue(node in i.inflow)
 
-    def test_create_graph(self):
-        graph = LocationGraph(load_data())
-        self.assertEqual(graph.length(), 4, "All items should be converted to graph")
-        self.all_connections_both_directions(graph)
+    # def test_create_graph(self):
+    #     graph = LocationGraph(load_data())
+    #     self.assertEqual(graph.length(), 4, "All items should be converted to graph")
+    #     self.all_connections_both_directions(graph)
 
-    def test_graph_node_ordering(self):
-        graph = LocationGraph(load_data())
-        node_list = list(graph.ascending())
-        for i in range(len(node_list) - 1):
-            self.assertLessEqual(node_list[i].altitude, node_list[i + 1].altitude)
+    # def test_graph_node_ordering(self):
+    #     graph = LocationGraph(load_data())
+    #     node_list = list(graph.ascending())
+    #     for i in range(len(node_list) - 1):
+    #         self.assertLessEqual(node_list[i].altitude, node_list[i + 1].altitude)
 
-    def test_node_conversion(self):
-        graph = LocationGraph([[0.1, 0.2]])
-        node = graph.last
+    # def test_node_conversion(self):
+    #     graph = LocationGraph([[0.1, 0.2]])
+    #     node = graph.last
 
-        self.assertEqual(node.altitude, 0.2)
-        self.assertEqual(node.flow, 0.0)
-        self.assertEqual(node.original_location, {(0, 1)})
-        self.assertEqual(len(node.inflow), 0)
-        self.assertEqual(len(node.outflow), 1)
-        self.assertEqual(graph.first, next(iter(node.outflow)))
-        self.all_connections_both_directions(graph)
+    #     self.assertEqual(node.altitude, 0.2)
+    #     self.assertEqual(node.flow, 0.0)
+    #     self.assertEqual(node.original_location, {(0, 1)})
+    #     self.assertEqual(len(node.inflow), 0)
+    #     self.assertEqual(len(node.outflow), 1)
+    #     self.assertEqual(graph.first, next(iter(node.outflow)))
+    #     self.all_connections_both_directions(graph)
 
     def test_node_merging(self):
         graph = LocationGraph([[0.1, 0.2], [0.1, 0.3]])
