e81026bb904c3f3248bd6db841fb2a7b4d0234d5
Andrew Houghton <houghtonandrew0@gmail.com>
2019-02-11 14:09:34 +1100

    Removed print statements from tests

--- algorithms/flood.py
+++ algorithms/flood.py
@@ -1,11 +1,13 @@
 from sortedcollections import SortedList
 
+
 def start_flood(lake):
     # add neighbours to sorted collection
     edge_of_flood = SortedList()
     edge_of_flood.update(lake.inflow)
 
-    next_lowest_neighbour = edge_of_flood.pop() # Once we look at an item we are done with it
+    # Once we look at an item we are done with it
+    next_lowest_neighbour = edge_of_flood.pop()
     while next_lowest_neighbour.altitude > lake.altitude and not lake.border:
         # merge node into next lowest neighbour
         next_lowest_neighbour.merge(lake)
@@ -19,5 +21,4 @@ def flood(nodes):
     # from lowest altitude to highest find the low points and flood them
     for node in nodes:
         if len(node.outflow) == 0 and not node.border:
-            print("low point found")
             start_flood(node)
