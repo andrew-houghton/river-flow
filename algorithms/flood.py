ed530e01aec206c12f196a0fbd0687d9e06e70eb
Andrew Houghton <houghtonandrew0@gmail.com>
2019-02-11 14:14:24 +1100

    Added better error messaging

--- algorithms/flood.py
+++ algorithms/flood.py
@@ -3,18 +3,21 @@ from sortedcollections import SortedList
 
 def start_flood(lake):
     # add neighbours to sorted collection
-    edge_of_flood = SortedList()
-    edge_of_flood.update(lake.inflow)
+    edge_of_lake = SortedList()
+    edge_of_lake.update(lake.inflow)
 
     # Once we look at an item we are done with it
-    next_lowest_neighbour = edge_of_flood.pop()
+    next_lowest_neighbour = edge_of_lake.pop()
     while next_lowest_neighbour.altitude > lake.altitude and not lake.border:
         # merge node into next lowest neighbour
         next_lowest_neighbour.merge(lake)
 
         # move to look at next node on the edge of the flood
         lake = next_lowest_neighbour
-        next_lowest_neighbour = edge_of_flood.pop()
+        try:
+            next_lowest_neighbour = edge_of_lake.pop()
+        except IndexError as e:
+            raise IndexError("No nodes left on the edge of the lake to add.") from e
 
 
 def flood(nodes):
